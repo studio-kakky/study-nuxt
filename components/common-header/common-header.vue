037988835474eb6e12c54827fe285bd1abe033ec
柿内暢昌 <nob@studio-kakky.com>
2021-02-05 16:51:30 +0900

    common-header

---
+++ components/common-header/common-header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="PageHeader">
+    <h1>StudyNuxt</h1>
+    <nav class="GlobalNav">
+      <ul class="GlobalNav_Items">
+        <li class="GlobalNav_Item"><a class="GlobalNav_Item_Link">Home</a></li>
+        <li class="GlobalNav_Item"><a class="GlobalNav_Item_Link">About</a></li>
+        <li class="GlobalNav_Item"><a class="GlobalNav_Item_Link">Blogs</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+
+@Component
+export class TestComponent extends Vue {}
+</script>
