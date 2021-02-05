3a0b9c168fe6a568ae96090335780e40e3fb6d4b
柿内暢昌 <nob@studio-kakky.com>
2021-02-05 17:18:55 +0900

    global navi追加

--- components/common-header/common-header.vue
+++ components/common-header/common-header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="PageHeader">
-    <h1>StudyNuxt</h1>
+    <h1 class="PageHeader_h">StudyNuxt</h1>
     <nav class="GlobalNav">
       <ul class="GlobalNav_Items">
         <li class="GlobalNav_Item"><a class="GlobalNav_Item_Link">Home</a></li>
@@ -15,5 +15,29 @@
 import { Vue, Component } from 'nuxt-property-decorator';
 
 @Component
-export class TestComponent extends Vue {}
+export default class CommonHeader extends Vue {}
 </script>
+
+<style lang="scss">
+.PageHeader {
+  width: 100%;
+  &_h {
+    text-align: center;
+    width: 100%;
+    font-size: 20px;
+    margin: 20px 0;
+  }
+}
+
+.GlobalNav {
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+  &_Items {
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+    padding: 0;
+  }
+}
+</style>
